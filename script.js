1eebedeb9ccda471295f96854178cf57986a6e58
mohb riaz <mohbriaz@gmail.com>
2023-06-28 13:48:39 +0530

    2nd commit

--- script.js
+++ script.js
@@ -1,74 +1,61 @@
-  //Create a IMG-CONTAINER element with class IMG
-  var imgContainer = document.createElement("div");
-  imgContainer.setAttribute("class", "img");
 
-  //Create a IMG element with SRC
-  var newImgElement = document.createElement("img");
-  newImgElement.src = preview;
+      // Product Preview Image 0
+  var photo0 = document.getElementById("one");
+  photo0.src = new URL('https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/08a7b230-ee8f-46c0-a945-4e835a3c01c01541402833619-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-1.jpg');
 
- // Append IMG into IMG-CONTAINER
- imgContainer.appendChild(newImgElement);
+  // Product Preview Image 1
+  var photo1 = document.getElementById("two");
+  photo1.src = new URL('https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/efc3d5b9-1bb3-4427-af53-7acae7af98951541402833591-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-2.jpg');
 
-   // Append IMG-CONATINER into CARD-LINK
-   cardLink.appendChild(imgContainer);
-   cardLink.appendChild(deatils);
- 
+  // Product Preview Image 2
+  var photo2 = document.getElementById("three");
+  photo2.src = three;
 
-// Product Preview Image 0
-var photo0 = document.getElementById("one");
-photo0.src = one;
+  // Product Preview Image 3
+  var photo3 = document.getElementById("four");
+  photo3.src = four;
 
-// Product Preview Image 1
-var photo1 = document.getElementById("two");
-photo1.src = two;
-
-// Product Preview Image 2
-var photo2 = document.getElementById("three");
-photo2.src = three;
-
-// Product Preview Image 3
-var photo3 = document.getElementById("four");
-photo3.src = four;
-
-// Product Preview Image 4
-var photo4 = document.getElementById("five");
-photo4.src = five;
+  // Product Preview Image 4
+  var photo4 = document.getElementById("five");
+  photo4.src = five;
 
 
-// ---------------- Change Preview Image OnClick -------
-function changeImage() {
+  // ---------------- Change Preview Image OnClick -------
+  function changeImage() {
     // Photo 0
     photo0.addEventListener("click", function() {
-      productImg.src = one;
+      mainImg.src = one;
     });
 
     // Photo 1
     photo1.addEventListener("click", function() {
-      productImg.src = two;
+      mainImg.src = new URL('https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/efc3d5b9-1bb3-4427-af53-7acae7af98951541402833591-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-2.jpg');
     });
+
     // Photo 2
     photo2.addEventListener("click", function() {
-      productImg.src = three;
+      mainImg.src = three;
     });
 
     // Photo 3
     photo3.addEventListener("click", function() {
-      productImg.src = four;
+      mainImg.src = four;
     });
 
     // Photo 4
     photo4.addEventListener("click", function() {
-      productImg.src = five;
+      mainImg.src = five;
     });
-
 
     // Toggle Active Class
     $(document).on("click", ".image img", function() {
-        $(this)
-          .addClass("active")
-          .siblings()
-          .removeClass("active");
-      });
-    changeImage();
+      $(this)
+        .addClass("active")
+        .siblings()
+        .removeClass("active");
+    });
+  
+  changeImage();
+}
 
   
